f384fd94e90cb6b5f5ad8a85e2954b5272cc070d
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2021-10-03 16:03:47 +0530

    Update openshot.sh

--- openshot.sh
+++ openshot.sh
@@ -1,5 +1,5 @@
 sudo apt install sysvbanner -y
-banner Open Shot Video Editor For Raspberry Pi By Tharuk
+banner Open Shot
 sudo apt update && apt upgrade -y
 sudo apt install openshot-qt -y
-banner Installation Successfully
+banner Done
